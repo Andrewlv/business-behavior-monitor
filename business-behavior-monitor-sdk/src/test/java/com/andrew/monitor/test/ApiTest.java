68aa76501483cb66fd344e64a8a8999192fe66a8
Andrewlv <2289380832@qq.com>
2024-08-23 17:40:37 +0800

    feat: project pom init

--- business-behavior-monitor-sdk/src/test/java/com/andrew/monitor/test/ApiTest.java
+++ business-behavior-monitor-sdk/src/test/java/com/andrew/monitor/test/ApiTest.java
@@ -1,9 +1,8 @@
 package com.andrew.monitor.test;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-@Slf4j
+
 public class ApiTest {
 
     @Test
--- business-behavior-monitor-test/src/main/java/com/andrew/monitor/Application.java
+++ business-behavior-monitor-test/src/main/java/com/andrew/monitor/Application.java
@@ -1,5 +1,9 @@
 package com.andrew.monitor;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 @Configurable
 public class Application {
