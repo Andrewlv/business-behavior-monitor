2330d192ad3050db0dc2e9a8c4fdd597aff2433b
Andrewlv <2289380832@qq.com>
2024-08-23 17:29:04 +0800

    feat: admin init

---
+++ business-behavior-monitor-admin/src/test/java/com/andrew/monitor/ApiTest.java
@@ -0,0 +1,15 @@
+package com.andrew.monitor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+
+@Slf4j
+public class ApiTest {
+
+    @Test
+    public void test_ognl() {
+
+    }
+
+}
